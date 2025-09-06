d29557cc0f02467f4c93adc37c76f7397dee8f62
Víctor Escalante <victor@qodeux.tech>
2025-09-06 17:10:50 -0600

    feat: agregar FrontwebLayout para la estructura de la página principal

---
+++ src/layouts/FrontwebLayout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router'
+import Footer from '../components/common/Footer'
+import Header from '../components/common/Header'
+
+const FrontwebLayout = () => {
+  return (
+    <main className='flex flex-col min-h-screen'>
+      <Header />
+      <section className='flex-grow container mx-auto px-4 py-8'>
+        <Outlet />
+      </section>
+      <Footer />
+    </main>
+  )
+}
+
+export default FrontwebLayout
