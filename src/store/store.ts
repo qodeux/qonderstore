e9bda1ae5d60f8a599f8c26640734fe440542b17
kevanton <kevindelic@gmail.com>
2025-10-06 11:27:20 -0600

    feat: agregar reducers de categorías y promociones al store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './slices/authSlice'
+import categoriesReducer from './slices/categoriesSlice'
+import promotionReducer from './slices/promoSlice'
+
 import uiReducer from './slices/uiSlice'
 
 const store = configureStore({
   reducer: {
     ui: uiReducer,
-    auth: authReducer
+    auth: authReducer,
+    categories: categoriesReducer,
+    promotions: promotionReducer
   }
 })
 
