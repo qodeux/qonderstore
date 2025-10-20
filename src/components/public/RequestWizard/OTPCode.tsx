dbd7ac6d079f7a48f99bee7e2ff39f18c7602d2c
Víctor Escalante <victor@qodeux.tech>
2025-10-20 01:56:09 -0600

    feat: implementar gestión de navegación en OTPCode; actualizar lógica para el botón de regresar

--- src/components/public/RequestWizard/OTPCode.tsx
+++ src/components/public/RequestWizard/OTPCode.tsx
@@ -1,10 +1,20 @@
 import { Button, InputOtp } from '@heroui/react'
+import { flushSync } from 'react-dom'
 import { Controller, useFormContext } from 'react-hook-form'
+import { useDispatch } from 'react-redux'
 import { useWizard } from 'react-use-wizard'
+import { setWizardNavDir } from '../../../store/slices/uiSlice'
 
 const OTPCode = () => {
   const { control } = useFormContext()
   const { previousStep, nextStep } = useWizard()
+  const dispatch = useDispatch()
+
+  const handlePrev = async () => {
+    // Marca intención de navegación SINCRÓNICAMENTE antes de cambiar de paso
+    flushSync(() => dispatch(setWizardNavDir(-1)))
+    await previousStep()
+  }
 
   return (
     <form className='flex w-full flex-col space-y-2  text-center'>
@@ -24,7 +34,7 @@ const OTPCode = () => {
 
       <p className='text-xs'>¿No recibiste el código? Solicita uno nuevo o comprueba tu número de teléfono.</p>
 
-      <Button variant='ghost' color='primary' size='md' className='w-full mt-2' onPress={previousStep}>
+      <Button variant='ghost' color='primary' size='md' className='w-full mt-2' onPress={handlePrev}>
         Regresar
       </Button>
     </form>
