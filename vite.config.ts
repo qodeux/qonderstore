05457b60078e0546a147877c243f3dda8db3aed2
Víctor Escalante <victor@qodeux.tech>
2025-10-24 19:04:13 -0600

    feat: simplificar la estructura de referencias en tsconfig y agregar configuración de alias en vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react(), tailwindcss()]
+  plugins: [svgr(), react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
