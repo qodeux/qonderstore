ff3e6e69dfed552247136e0ac59ee2044d5c5da7
kevanton <kevindelic@gmail.com>
2025-11-01 01:30:36 -0600

    feat: agregar lógica para mostrar subcategorías en la vista de categoría

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -2,6 +2,7 @@ import { Button, Input, Select, SelectItem } from '@heroui/react'
 import { ArrowDownWideNarrow, ChevronLeft } from 'lucide-react'
 import { useSelector } from 'react-redux'
 import { useNavigate, useParams } from 'react-router'
+import CategoryItem from '../components/store/CategoryItem'
 import ProductItem from '../components/store/ProductItem'
 import { selectProductsWithBestPromo } from '../store/selectors/productsWithPromo'
 import type { RootState } from '../store/store'
@@ -13,8 +14,11 @@ const Category = () => {
   const navigate = useNavigate()
 
   const featuredProducts = products.filter((p) => p.category === categories.find((c) => c.slug_id == slug)?.name && p.featured === true)
-
   const allProducts = products.filter((p) => p.category === categories.find((c) => c.slug_id == slug)?.name && p.featured === false)
+
+  const hasSubcategories = categories.some((c) => c.parent === categories.find((cat) => cat.slug_id == slug)?.id)
+
+  const subcategories = categories.filter((c) => c.parent === categories.find((cat) => cat.slug_id == slug)?.id)
 
   return (
     <div className='w-full container mx-auto px-4 py-10 flex flex-col gap-12'>
@@ -27,6 +31,20 @@ const Category = () => {
         <h2 className='text-5xl font-bold text-center'>Categoría {categories.find((c) => c.slug_id == slug)?.name}</h2>
         <div></div>
       </header>
+
+      {hasSubcategories && (
+        <section>
+          <header>
+            <h3 className='text-3xl font-bold mb-8'>Subcategorías</h3>
+          </header>
+          <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-6'>
+            {subcategories.map((cat) => (
+              <CategoryItem category={cat} />
+            ))}
+          </div>
+        </section>
+      )}
+
       {featuredProducts.length > 0 && (
         <section>
           <header>
