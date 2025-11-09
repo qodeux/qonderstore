631579d103d9339fb12d6cf0b6338ecd97e843ae
Víctor Escalante <victor@qodeux.tech>
2025-11-08 23:20:07 -0600

    feat: agregar lógica para manejar entradas de tipo Record en la función toTypeRecord, mejorando la flexibilidad del mapeo

--- src/types/helpers.ts
+++ src/types/helpers.ts
@@ -1,3 +1,28 @@
+import type { KeyLike, MapInput, TypeValue } from '../components/common/DataTable'
+
 export function toRecord<const T extends readonly { key: PropertyKey; label: string }[]>(arr: T) {
   return Object.fromEntries(arr.map((a) => [a.key, a.label])) as Record<T[number]['key'], string>
+}
+
+export function toTypeRecord<K extends KeyLike, V>(input: MapInput<K, V>): Record<K, TypeValue<V>> {
+  if (Array.isArray(input)) {
+    return input.reduce(
+      (acc, { key, label, color }) => {
+        ;(acc as any)[key] = { label, color }
+        return acc
+      },
+      {} as Record<K, TypeValue<V>>
+    )
+  }
+
+  // Es un Record
+  const out: Record<K, TypeValue<V>> = {} as any
+  for (const [k, v] of Object.entries(input) as [K, V | TypeValue<V>][]) {
+    if (typeof v === 'object' && v !== null && 'label' in v) {
+      out[k] = v as TypeValue<V>
+    } else {
+      out[k] = { label: v as V }
+    }
+  }
+  return out
 }
