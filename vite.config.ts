aaad61f7bddc6498b838a1763f460d88ccadfd9b
Víctor Escalante <victor@qodeux.tech>
2025-11-09 04:40:32 -0600

    feat: agregar versión de la aplicación en el pie de página administrativo

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
+import pkg from './package.json'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svgr(), react(), tailwindcss()],
@@ -10,5 +11,8 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version)
   }
 })
