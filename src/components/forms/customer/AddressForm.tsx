90faed075d75e31b0fa616df705974b7f3dcb97b
Víctor Escalante <victor@qodeux.tech>
2025-11-11 06:17:54 -0600

    feat: agregar componente AddressForm que incluye AddressFinder

---
+++ src/components/forms/customer/AddressForm.tsx
@@ -0,0 +1,11 @@
+import AddressFinder from '../../common/AddressFinder'
+
+const AddressForm = () => {
+  return (
+    <form>
+      <AddressFinder />
+    </form>
+  )
+}
+
+export default AddressForm
