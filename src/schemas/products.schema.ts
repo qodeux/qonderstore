8d331c38a2b6248403ebecb1a3cb93dc0602f15f
Víctor Escalante <victor@qodeux.tech>
2025-11-12 13:09:38 -0600

    fix: cambiar tipo de dato de 'brand' a número en el esquema de productos

--- src/schemas/products.schema.ts
+++ src/schemas/products.schema.ts
@@ -340,7 +340,7 @@ export const productSchema = z.object({
   max_sale: z.number().optional(),
   featured: z.boolean(),
   is_active: z.boolean(),
-  brand: z.string().optional(),
+  brand: z.number().optional(),
   created_at: z.string(),
   images: z.array(z.string()).optional(),
   main_image: z.string().optional()
