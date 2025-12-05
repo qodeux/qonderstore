159f75c6515054c9e95438abc0d5e3091c6c3fc4
kevanton <kevindelic@gmail.com>
2025-12-05 07:52:45 -0600

    feat: agregar modal de configuración y formulario para métodos de pago

---
+++ src/components/modals/admin/ConfigModal.tsx
@@ -0,0 +1,121 @@
+import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from '@heroui/react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useEffect } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { brandInputSchema } from '../../../schemas/brand.schema'
+import { productService } from '../../../services/productService'
+import { useAppSelector } from '../../../store/store'
+import PaymentMethodsForm from '../../forms/admin/PaymentMethodsForm'
+
+type Props = {
+  isOpen: boolean
+  onOpenChange: () => void
+}
+
+const PaymentMethodsModal = ({ isOpen, onOpenChange }: Props) => {
+  const { isEditing } = useAppSelector((state) => state.ui)
+  const { selectedBrand } = useAppSelector((state) => state.products)
+
+  const paymentMethodForm = useForm({
+    resolver: zodResolver(brandInputSchema),
+    shouldUnregister: false,
+    mode: 'all',
+    reValidateMode: 'onChange',
+
+    defaultValues: {
+      name: '',
+      slug: '',
+      color: undefined
+    }
+  })
+
+  const {
+    formState: { isDirty, errors }
+  } = paymentMethodForm
+
+  const buildFormValues = () => {
+    if (isEditing) {
+      return {
+        name: selectedBrand?.name ?? '',
+        color: selectedBrand?.color ?? null,
+        slug: selectedBrand?.slug ?? ''
+      }
+    } else {
+      return {
+        name: '',
+        color: null,
+        slug: ''
+      }
+    }
+  }
+
+  const handleSubmitBrand = async () => {
+    const isValid = await paymentMethodForm.trigger()
+    if (!isValid) return
+    let formData = paymentMethodForm.getValues()
+    let transaction
+
+    if (isEditing && selectedBrand?.id) {
+      formData = { ...formData, id: selectedBrand.id }
+      transaction = await productService.updateBrand(formData)
+    } else {
+      transaction = await productService.createBrand(formData)
+    }
+
+    if (!transaction.error && transaction) {
+      onOpenChange()
+      paymentMethodForm.reset()
+    } else {
+      console.error('Error al guardar categoría')
+      console.error(transaction.error)
+
+      if (transaction.error.code === '23505') {
+        // Manejo de error: clave duplicada
+        const details = transaction.error?.details ?? ''
+        if (details.includes('Key (name)')) {
+          paymentMethodForm.setError('name', { message: 'El nombre ya existe' })
+        } else if (details.includes('Key (slug)')) {
+          paymentMethodForm.setError('slug', { message: 'El slug ya existe' })
+        } else {
+          console.error('Error desconocido:', details)
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (isOpen) {
+      // Al abrir, setea valores actuales
+      paymentMethodForm.reset(buildFormValues())
+    }
+  }, [isOpen, selectedBrand, paymentMethodForm]) // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <Modal isOpen={isOpen} onOpenChange={onOpenChange} size='sm' backdrop='blur'>
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader className='flex flex-col gap-1'>{isEditing ? 'Editar' : 'Agregar'} método de pago</ModalHeader>
+            <ModalBody>
+              <FormProvider {...paymentMethodForm}>
+                <PaymentMethodsForm />
+              </FormProvider>
+            </ModalBody>
+            <ModalFooter>
+              <Button color='danger' variant='light' onPress={onClose}>
+                Cerrar
+              </Button>
+              {(!isEditing || isDirty) && (
+                <Button color='primary' onPress={handleSubmitBrand} isDisabled={Object.keys(errors).length > 0}>
+                  Aceptar
+                </Button>
+              )}
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  )
+}
+
+export default PaymentMethodsModal
--- src/pages/admin/config/PaymentMethods.tsx
+++ src/pages/admin/config/PaymentMethods.tsx
@@ -10,7 +10,8 @@ import {
   TableCell,
   TableColumn,
   TableHeader,
-  TableRow
+  TableRow,
+  useDisclosure
 } from '@heroui/react'
 import { Building2, CreditCard, EllipsisVertical } from 'lucide-react'
 import { useEffect, useMemo, useState } from 'react'
@@ -18,6 +19,8 @@ import type { ColumnDef } from '../../../components/common/DataTable'
 import { ToolbarTable, type ToolbarCriteria } from '../../../components/common/ToolbarTable'
 import { useAppSelector } from '../../../store/store'
 import { applyToolbarFilters } from '../../../utils/toolbarFilters'
+
+import ConfigModal from '../../../components/modals/admin/ConfigModal'
 
 const getMethodLabel = (type: string) => {
   switch (type) {
@@ -92,7 +95,12 @@ const PaymentMethods = () => {
     return applyToolbarFilters(rows, ['details'], criteria)
   }, [rows, criteria])
 
-  const handleAddMethod = () => console.log('Agregar método')
+  const { isOpen: isOpenConfig, onOpenChange: onOpenChangeConfig } = useDisclosure()
+
+  const handleAddMethod = () => {
+    onOpenChangeConfig()
+    console.log('Agregar método')
+  }
   const handleEditMethod = (row: Row) => console.log('Editar método', row)
   const handleDelete = (row: Row) => console.log('Eliminar método', row)
   const handleToggle = (row: Row, v: boolean) => console.log('Cambiar estado:', row, v)
@@ -115,102 +123,105 @@ const PaymentMethods = () => {
   }, [rows])
 
   return (
-    <section className='flex h-full flex-col gap-4'>
-      <section className='space-y-4'>
-        <ToolbarTable<Row>
-          rows={rows}
-          filters={[
-            {
-              label: 'Tipo de pago',
-              column: 'payment_method',
-              multiple: true,
-              optionsMap: { card: 'Tarjeta', bank_transfer: 'Transferencia bancaria' }
-            }
-          ]}
-          buttons={[{ label: 'Agregar método', onPress: handleAddMethod, color: 'primary' }]}
-          onCriteriaChange={setCriteria}
-        />
+    <>
+      <section className='flex h-full flex-col gap-4'>
+        <section className='space-y-4'>
+          <ToolbarTable<Row>
+            rows={rows}
+            filters={[
+              {
+                label: 'Tipo de pago',
+                column: 'payment_method',
+                multiple: false,
+                optionsMap: { card: 'Tarjeta', bank_transfer: 'Transferencia bancaria' }
+              }
+            ]}
+            buttons={[{ label: 'Agregar método', onPress: handleAddMethod, color: 'primary' }]}
+            onCriteriaChange={setCriteria}
+          />
+        </section>
+
+        <Table aria-label='Tabla de métodos de pago' className='bg-white shadow-sm rounded-lg overflow-hidden' selectionMode='single'>
+          <TableHeader columns={columns}>
+            {(column) => (
+              <TableColumn key={String(column.key)} allowsSorting={!!column.allowsSorting} align={column.align ?? 'start'}>
+                {column.label}
+              </TableColumn>
+            )}
+          </TableHeader>
+
+          <TableBody emptyContent={'Sin resultados'} items={filteredRows}>
+            {(item: Row) => {
+              const { payment_method, details } = item
+              const { wrapper, icon } = getMethodIconStyles(payment_method)
+
+              return (
+                <TableRow key={`${payment_method}-${details.account}`}>
+                  {/* MÉTODO */}
+                  <TableCell>
+                    <div className='flex items-center gap-3'>
+                      <div className={`flex h-10 w-10 items-center justify-center rounded-full ${wrapper}`}>
+                        {getMethodIcon(payment_method, icon)}
+                      </div>
+                      <div>
+                        <p className='font-medium text-gray-800'>{getMethodLabel(payment_method)}</p>
+                        {/* <p className='text-xs text-gray-500'>({type})</p> */}
+                      </div>
+                    </div>
+                  </TableCell>
+
+                  {/* DETALLES */}
+                  <TableCell>
+                    <div className='text-xs text-gray-600 space-y-1'>
+                      <p>
+                        <span className='font-semibold'>Banco:</span> {banks.find((b) => b.id === details.bank)?.full_name}
+                      </p>
+                      <p>
+                        <span className='font-semibold'>Cuenta:</span> {details.account}
+                      </p>
+                      <p>
+                        <span className='font-semibold'>Titular:</span> {details.holder_name}
+                      </p>
+                    </div>
+                  </TableCell>
+
+                  {/* ESTADO */}
+                  <TableCell>
+                    <div className='flex justify-center'>
+                      <Switch
+                        size='sm'
+                        // isSelected={!!active} // más adelante vendrá de la BD
+                        onValueChange={(v) => handleToggle(item, v)}
+                      />
+                    </div>
+                  </TableCell>
+
+                  {/* ACCIONES */}
+                  <TableCell>
+                    <Dropdown>
+                      <DropdownTrigger>
+                        <Button variant='bordered' isIconOnly size='sm'>
+                          <EllipsisVertical />
+                        </Button>
+                      </DropdownTrigger>
+                      <DropdownMenu aria-label='Acciones'>
+                        <DropdownItem key='__edit' onPress={() => handleEditMethod(item)}>
+                          Editar
+                        </DropdownItem>
+                        <DropdownItem key='__delete_req' className='text-danger' color='danger' onPress={() => handleDelete(item)}>
+                          Eliminar
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </Dropdown>
+                  </TableCell>
+                </TableRow>
+              )
+            }}
+          </TableBody>
+        </Table>
       </section>
-
-      <Table aria-label='Tabla de métodos de pago' className='bg-white shadow-sm rounded-lg overflow-hidden' selectionMode='single'>
-        <TableHeader columns={columns}>
-          {(column) => (
-            <TableColumn key={String(column.key)} allowsSorting={!!column.allowsSorting} align={column.align ?? 'start'}>
-              {column.label}
-            </TableColumn>
-          )}
-        </TableHeader>
-
-        <TableBody emptyContent={'Sin resultados'} items={filteredRows}>
-          {(item: Row) => {
-            const { payment_method, details } = item
-            const { wrapper, icon } = getMethodIconStyles(payment_method)
-
-            return (
-              <TableRow key={`${payment_method}-${details.account}`}>
-                {/* MÉTODO */}
-                <TableCell>
-                  <div className='flex items-center gap-3'>
-                    <div className={`flex h-10 w-10 items-center justify-center rounded-full ${wrapper}`}>
-                      {getMethodIcon(payment_method, icon)}
-                    </div>
-                    <div>
-                      <p className='font-medium text-gray-800'>{getMethodLabel(payment_method)}</p>
-                      {/* <p className='text-xs text-gray-500'>({type})</p> */}
-                    </div>
-                  </div>
-                </TableCell>
-
-                {/* DETALLES */}
-                <TableCell>
-                  <div className='text-xs text-gray-600 space-y-1'>
-                    <p>
-                      <span className='font-semibold'>Banco:</span> {banks.find((b) => b.id === details.bank)?.full_name}
-                    </p>
-                    <p>
-                      <span className='font-semibold'>Cuenta:</span> {details.account}
-                    </p>
-                    <p>
-                      <span className='font-semibold'>Titular:</span> {details.holder_name}
-                    </p>
-                  </div>
-                </TableCell>
-
-                {/* ESTADO */}
-                <TableCell>
-                  <div className='flex justify-center'>
-                    <Switch
-                      size='sm'
-                      // isSelected={!!active} // más adelante vendrá de la BD
-                      onValueChange={(v) => handleToggle(item, v)}
-                    />
-                  </div>
-                </TableCell>
-
-                {/* ACCIONES */}
-                <TableCell>
-                  <Dropdown>
-                    <DropdownTrigger>
-                      <Button variant='bordered' isIconOnly size='sm'>
-                        <EllipsisVertical />
-                      </Button>
-                    </DropdownTrigger>
-                    <DropdownMenu aria-label='Acciones'>
-                      <DropdownItem key='__edit' onPress={() => handleEditMethod(item)}>
-                        Editar
-                      </DropdownItem>
-                      <DropdownItem key='__delete_req' className='text-danger' color='danger' onPress={() => handleDelete(item)}>
-                        Eliminar
-                      </DropdownItem>
-                    </DropdownMenu>
-                  </Dropdown>
-                </TableCell>
-              </TableRow>
-            )
-          }}
-        </TableBody>
-      </Table>
-    </section>
+      <ConfigModal isOpen={isOpenConfig} on onOpenChange={onOpenChangeConfig} />
+    </>
   )
 }
 
