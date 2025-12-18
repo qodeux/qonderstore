6f04891c41603fe5ecaec8d439bb4947cecbf527
Víctor Escalante <victor@qodeux.tech>
2025-12-18 08:36:04 -0600

    feat: agregar lógica de limpieza del carrito y mejorar el mensaje de confirmación de pedido en la página de confirmación de orden

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -8,7 +8,6 @@ import AdminSidebar from '../components/common/AdminSidebar'
 import Header from '../components/common/Header'
 import AccountModal from '../components/modals/admin/AccountModal'
 import CartSidebar from '../components/store/CartSidebar'
-import { useConfig } from '../hooks/useConfig'
 import { useDeviceScreen } from '../hooks/useDeviceScreen'
 import { useProviders } from '../hooks/useProviders'
 import { useRequests } from '../hooks/useRequests'
@@ -22,7 +21,7 @@ const AdminLayout = () => {
   useRequests()
   useSupplyOrders()
   useStoreOrders()
-  useConfig()
+
   const dispatch = useDispatch<AppDispatch>()
   const contentRef = useRef<HTMLDivElement>(null)
   const location = useLocation()
--- src/pages/store/OrderConfirmation.tsx
+++ src/pages/store/OrderConfirmation.tsx
@@ -1,14 +1,35 @@
 import { useParams } from 'react-router'
+import { clearCart } from '../../store/slices/cartSlice'
+import { useAppDispatch, useAppSelector } from '../../store/store'
 
 const OrderConfirmation = () => {
   const { oid } = useParams()
+  const dispatch = useAppDispatch()
+
+  dispatch(clearCart())
+
+  const { paymentMethods } = useAppSelector((state) => state.config)
+
+  if (!oid) {
+    return <p>ID de orden no proporcionado.</p>
+  }
 
   return (
     <div className='container mx-auto p-8'>
-      <h1>Tu orden ha sido confirmada</h1>
-      <p>El ID de tu orden es: {oid}</p>
+      <h1 className='text-xl font-semibold'>Tu pedido ha sido recibido</h1>
+      <p>El ID de tu orden es: {oid.split('-')[0]}</p>
 
-      <p>Puedes pagar haciendo una transferencia a cualquiera de las siguientes cuentas o en efectivo en tiendas de conveniencia.</p>
+      <p>
+        Puedes pagar haciendo una transferencia o en efectivo depositando en tiendas de conveniencia a cualquiera de las siguientes cuentas:
+      </p>
+
+      <ul className='list-disc list-inside mb-4'>
+        {paymentMethods?.map((method) => (
+          <li key={method.id}>
+            {method.type}: {method.account}
+          </li>
+        ))}
+      </ul>
 
       <p>Una vez realizado el pago , sube su comprobante aquí, o en tu tus ordenes</p>
     </div>
